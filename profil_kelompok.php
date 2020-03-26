0bafdb89ef20b8f0626c7676cd521fa5818a1a07
laitsky <vincent.diamond15@gmail.com>
2020-03-26 13:03:08 +0700

    profil_kelompok.php done

--- profil_kelompok.php
+++ profil_kelompok.php
@@ -5,5 +5,14 @@ require_once "header.php";
     <div class="text-center">
         <h1>Profil Kelompok</h1>
         <h4>Susunan anggota kelompok perancang website ini:</h4>
+        <div class="container pt-3" style="max-width: 480px;">
+            <ul>
+                <li>Ignatius Giovanni Jevon / 00000026850</li>
+                <li>Vincent Diamond / 00000027047</li>
+                <li>Andrew Leonardi / 00000027137</li>
+                <li>Samuel / 00000027881</li>
+                <li>Liana / 00000027963</li>
+            </ul>
+        </div>
     </div>
 </div>
